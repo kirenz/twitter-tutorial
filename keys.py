b987ffce9c47162249906a7bf127c3d26af0d5f9
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-10-14 16:16:02 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -9,7 +9,11 @@ This file contains your Twitter keys
 """
 
 consumer_key='insert your API key'
+
 consumer_secret='insert your API secret'
+
 access_token='insert your access token'
+
 access_token_secret='insert your access token secret'
+
 bearer_token='insert your bearer token'
